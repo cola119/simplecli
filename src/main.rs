41a63a5ea527c4062af673a785ad2a359444b6c9
cola119 <k.cola119@ezweb.ne.jp>
2020-09-26 16:51:48 +0900

    load text data

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Kohei Ueno",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let render = BufReader::new(f);
+
+        for line in render.lines() {
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    } else {
+        println!("No file is specified");
+    }
+}
